16f7b2a2af871e8f19ef028e028c08e9553d6417
Erik Alonso <erikeah@protonmail.com>
2024-12-21 17:33:22 +0000

    feat: intermediatesHook

--- example/flake.nix
+++ example/flake.nix
@@ -5,35 +5,62 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     intermediates-nix.url = "github:erikeah/intermediates-nix";
+    intermediates-nix-hook.url = "github:erikeah/intermediates-nix?dir=hook";
     promotedIntermediates.url = "path:./promotedIntermediates";
   };
 
-  outputs = inputs@{ flake-parts, intermediates-nix, ... }:
+  outputs =
+    inputs@{ flake-parts, intermediates-nix, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
           packages = intermediates-nix.lib.withIntermediatesOnSet inputs'.promotedIntermediates.packages {
-              default = { requestedIntermediates, ... }: with pkgs; stdenv.mkDerivation {
-                  name = "example";
-                  description = "bla bla... bla";
-                  dontUnpack = true;
-                  dontConfigure = true;
-                  outputs = [ "out" "intermediates" ];
-                  nativeBuildInputs = [];
-                  buildPhase = ''
-                    cp -r ${requestedIntermediates} $intermediates
-                    find $intermediates -exec chmod u+w {} +
-                    find $intermediates -exec touch -d '1970-01-01T00:00:00Z' {} +
+            default =
+              { requestedIntermediates, ... }:
+              with pkgs;
+              stdenv.mkDerivation {
+                name = "example";
+                description = "bla bla... bla";
+                dontUnpack = true;
+                dontConfigure = true;
+                env = { inherit requestedIntermediates; };
+                outputs = [
+                  "out"
+                  "intermediates"
+                ];
+                nativeBuildInputs = [ inputs'.intermediates-nix-hook.packages.intermediatesHook ];
+                buildPhase = ''
+                  runHook preBuild
 
-                    date >> $intermediates/message
-                  '';
-                  installPhase = ''
-                    mkdir $out/bin -p
-                    echo -e '#!/bin/sh\ncat' "$intermediates/message" > $out/bin/intermediates-message
-                    chmod 755 $out/bin/intermediates-message
-                  '';
+                  date >> $intermediates/message
+
+                  runHook postBuild
+                '';
+                installPhase = ''
+                  runHook preInstall
+
+                  mkdir $out/bin -p
+                  echo -e '#!/bin/sh\ncat' "$intermediates/message" > $out/bin/intermediates-message
+                  chmod 755 $out/bin/intermediates-message
+
+                  runHook postInstall
+                '';
               };
           };
-      };
+        };
     };
 }
